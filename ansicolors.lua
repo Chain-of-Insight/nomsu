d477865b92ab840d459eb9b5be91b70bffde7a7b
Bruce Hill <bitbucket@bruce-hill.com>
2017-10-02 19:31:22 -0700

    Added ansi colors to repo.

---
+++ ansicolors.lua
@@ -0,0 +1,37 @@
+local colors = {
+    -- attributes
+    reset = 0,
+    clear = 0,
+    bright = 1,
+    dim = 2,
+    underscore = 4,
+    blink = 5,
+    reverse = 7,
+    hidden = 8,
+
+    -- foreground
+    black = 30,
+    red = 31,
+    green = 32,
+    yellow = 33,
+    blue = 34,
+    magenta = 35,
+    cyan = 36,
+    white = 37,
+
+    -- background
+    onblack = 40,
+    onred = 41,
+    ongreen = 42,
+    onyellow = 43,
+    onblue = 44,
+    onmagenta = 45,
+    oncyan = 46,
+    onwhite = 47,
+}
+
+local _M = {}
+for c, v in pairs(colors) do
+    _M[c] = string.char(27)..("[%dm"):format(v)
+end
+return _M
