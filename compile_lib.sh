1083273b9f422783a0976cba9ccd4316fd4c3860
Bruce Hill <bitbucket@bruce-hill.com>
2017-10-13 16:10:23 -0700

    Added -f option to force rebuilds (for when the compiler changes, not the lib files).

--- compile_lib.sh
+++ compile_lib.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+FLUSH=false
+while getopts ":f" opt; do
+  case $opt in
+    f)
+      FLUSH=true
+      echo "flushing..."
+      ;;
+  esac
+done
+if [ "$FLUSH" = true ] ; then
+    for file in $(find lib/ -name "*.nom.lua") ; do
+        rm $file
+    done
+fi
 for file in $(find lib/ -name "*.nom") ; do
     luafile="$file.lua"
     if [ ! -e "$luafile" ] || [ "$file" -nt "$luafile" ] ; then
